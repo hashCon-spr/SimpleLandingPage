528bdf7a957f68891d1cd8a9305a3bd8653637f9
getAnonymous <137413881+hashCon-spr@users.noreply.github.com>
2024-05-14 19:23:18 +0700

    New Updated

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ if(window.innerWidth < 700)
    console.log("mobile view")
 }
 else {
+   $(".mobile-bar").remove()
+   console.log("mobile bar removed")
    console.log("default view")
 }
-
-// scroll effect
-// $(".mobile-bar").remove()
